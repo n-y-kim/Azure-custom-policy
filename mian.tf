0748f104ac62fe9010c6b24dda4eeb6f463c127b
n-y-kim <nyn2265@gmail.com>
2024-03-07 10:43:06 +0900

    Add tf files to test policy

---
+++ mian.tf
@@ -0,0 +1,29 @@
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "East US"
+}
+
+resource "azurerm_network_security_group" "example" {
+  name                = "example-nsg"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+}
+
+resource "azurerm_virtual_network" "example" {
+  name                = "example-network"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  address_space       = ["10.0.0.0/16"]
+}
+
+resource "azurerm_subnet" "example" {
+  name                 = "example-subnet"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+# resource "azurerm_subnet_network_security_group_association" "example" {
+#   subnet_id                 = azurerm_subnet.example.id
+#   network_security_group_id = azurerm_network_security_group.example.id
+# }
